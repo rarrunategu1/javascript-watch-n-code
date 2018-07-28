9926f185b5ad0be109cb7c62972ffa757dd40367
Rose Parker <parker.rose@ymail.com>
2018-07-28 14:43:16 +0000

    Lesson 6 - Thinking in code

--- todo_list.js
+++ todo_list.js
@@ -502,6 +502,61 @@ todoList.displayTodos();  //returns
 ( ) first
 ( ) second
 
-todoList.toggleCompleted(0); //
+todoList.toggleCompleted(0); //returns
 (x) first
 
+/* V6 THINKING IN CODE
+
+V6 Requirements
+ .toggleAll: if everything's true, make everything false.
+ toggleAll: Otherwise, make everything true. */
+ 
+//.toggleAll: if everything's true, make everything false.
+
+ toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    // get number of completed todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    //if everything's true, make it false
+    if (completedTodos === totalTodos) {
+      for (var i =0; i <totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    }
+    this.displayTodos();
+  }
+  
+  //after all items have been togglecompleted and you do run todoList.toggleAll();  it will uncheck everything
+  
+  
+  
+  //toggleAll: Otherwise, make everything true. */
+  
+  
+ /* toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    // get number of completed todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    //if everything's true, make it false
+    if (completedTodos === totalTodos) {
+      for (var i =0; i <totalTodos; i++) {
+        this.todos[i].completed = false;
+      } */
+      //CASE 2: Other wise, make everything true.
+    }else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
+  }
